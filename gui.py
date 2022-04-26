7c8eb89eccc1144797a29cdd680e8ddbc38959a5
steven-horn <shorn.5294@gmail.com>
2022-04-25 23:56:10 -0500

    Added all widgets to init and placed all the classes widgets into hidden. Added fifth class.

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 class Gui:
     def __init__(self, window):
-        """Initial screen"""
+        """Initial screen and all widgets. Most of the widgets are initially hidden from the user with pack_forget()"""
         self.__number_of_classes = 1
         self.window = window
         self.window.geometry('300x100')
@@ -15,21 +15,58 @@ class Gui:
         self.welcome_label.pack()
         self.frame_top.pack()
 
-        self.frame_second = Frame(self.window)
-        self.class_entry = Entry(self.frame_second)
-        self.button_add_class = Button(self.frame_second, text='Add Class', command=self.clicked)
+        self.frame_one_class = Frame(self.window)
+        self.class_one_label = Label(self.frame_one_class, text='Temp')
+        self.grade_one_label = Label(self.frame_one_class, text='Temp')
+        self.class_one_button = Button(self.frame_one_class, text='Add grade')
+        self.class_one_label.pack_forget()
+        self.grade_one_label.pack_forget()
+        self.class_one_button.pack_forget()
+
+        self.frame_two_class = Frame(self.window)
+        self.class_two_label = Label(self.frame_two_class, text='Temp')
+        self.grade_two_label = Label(self.frame_two_class, text='Temp')
+        self.class_two_button = Button(self.frame_two_class, text='Add grade')
+        self.class_two_label.pack_forget()
+        self.grade_two_label.pack_forget()
+        self.class_two_button.pack_forget()
+
+        self.frame_three_class = Frame(self.window)
+        self.class_three_label = Label(self.frame_three_class, text='Temp')
+        self.grade_three_label = Label(self.frame_three_class, text='Temp')
+        self.class_three_button = Button(self.frame_three_class, text='Add grade')
+        self.class_three_label.pack_forget()
+        self.grade_three_label.pack_forget()
+        self.class_three_button.pack_forget()
+
+        self.frame_four_class = Frame(self.window)
+        self.class_four_label = Label(self.frame_four_class, text='Temp')
+        self.grade_four_label = Label(self.frame_four_class, text='Temp')
+        self.class_four_button = Button(self.frame_four_class, text='Add grade')
+        self.class_four_label.pack_forget()
+        self.grade_four_label.pack_forget()
+        self.class_four_button.pack_forget()
+
+        self.frame_five_class = Frame(self.window)
+        self.class_five_label = Label(self.frame_five_class, text='Temp')
+        self.grade_five_label = Label(self.frame_five_class, text='Temp')
+        self.class_five_button = Button(self.frame_five_class, text='Add grade')
+        self.class_five_label.pack_forget()
+        self.grade_five_label.pack_forget()
+        self.class_five_button.pack_forget()
+
+        self.frame_add_button = Frame(self.window)
+        self.class_entry = Entry(self.frame_add_button)
+        self.button_add_class = Button(self.frame_add_button, text='Add Class', command=self.clicked)
         self.class_entry.pack(side='left', padx=10)
         self.button_add_class.pack(side='right', padx=10)
-        self.frame_second.pack()
+        self.frame_add_button.pack()
 
     def clicked(self):
         """Function to set the screen"""
         if self.__number_of_classes == 1:
             self.one_class()
             self.__number_of_classes += 1
-            self.frame_second.destroy()
-            self.button_add_class.destroy()
-            self.class_entry.destroy()
         elif self.__number_of_classes == 2:
             self.two_classes()
             self.__number_of_classes += 1
@@ -69,3 +106,6 @@ class Gui:
     def four_classes(self):
         """Screen when we have created four grade books"""
         self.window.geometry('400x400')
+
+    def five_classes(self):
+        """Screen when we have created four grade books"""
